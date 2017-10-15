375c1116799a1b04b94356dfa58f0b0117154bb7
Luke-mc <luke_mcglynn@yahoo.com>
2017-10-14 18:39:23 -1000

    testing routes and checking layout of data

--- models/Users.js
+++ models/Users.js
@@ -1,24 +1,19 @@
 const sequelize = require('sequelize');
-const User_role = require('./User_role');
+
 
 
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
     email: { type: DataTypes.TEXT, allowNull: false},
-    password: { type: DataTypes.TEXT, allowNull: false}
+    password: { type: DataTypes.TEXT, allowNull: false},
+    user_role: { type: DataTypes.TEXT, allowNull: false}
+  },{
+    classMethods: {
+      associate: function(models) {
+
+      }
+    }
   });
-
-  Users.associate = function(models) {
-
-    Users.hasOne(models.User_role,
-      {
-        foreignKey: {
-          name: 'user_id',
-          allowNull: false
-        }
-      });
-
-  };
 
   return Users;
 };
