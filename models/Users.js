d68fdc4b4a09708d4f6877e7b5c3908098a3875e
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-10-16 11:25:31 -1000

    added first and last name

--- models/Users.js
+++ models/Users.js
@@ -4,6 +4,8 @@ const sequelize = require('sequelize');
 
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
+    firstName: { type: DataTypes.TEXT, allowNull: false},
+    lastName: { type: DataTypes.TEXT, allowNull: false},
     email: { type: DataTypes.TEXT, allowNull: false},
     password: { type: DataTypes.TEXT, allowNull: false},
     user_role: { type: DataTypes.TEXT, allowNull: false}
