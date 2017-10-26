92730c154243fde0e4e5f2257a12150e85890037
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-10-25 19:53:53 -1000

    passport and bcrypt working

--- api/users/index.js
+++ api/users/index.js
@@ -2,56 +2,60 @@ const express = require('express');
 const router = express.Router();
 const { Comments, Images, Pins, Projects, Schematics, Users } = require('../../models');
 
-// const passport = require('passport');
-// const LocalStrategy = require('passport-local').Strategy;
-// const bcrypt = require('bcrypt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
 
-// const saltRound = 10;
 
-// // Create/add new user to database
-// router.route('/signup')
-//   .post( (req, res) => {
-//     bcrypt.genSalt(saltRound)
-//       .then( (salt) => {
-//         bcrypt.hash(req.body.password, salt)
-//           .then( (hash) => {
-//             console.log(hash);
-//             User.create({
-//               email: req.body.email,
-//               password: hash
-//             }).then( () => {
-//               console.log('Inserted new user');
-//               res.end();
-//             }).catch( (err) => {
-//               console.log(err);
-//             });
-//           });
-//       })
-//       .catch( (err) => {
-//         console.log(err);
-//       });
-//     res.redirect('/');
-//   });
+const saltRound = 10;
 
+// Create/add new user to database
 router.route('/users')
   .post( (req, res) => {
-    return Users.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
-      user_role: req.body.user_role
-    })
-    .then( (user) => {
-      return res.json(user);
-    });
-  })
-  .get( (req, res) => {
-    Users.findAll()
-    .then( (users) => {
-      return res.json(users);
-    });
+    bcrypt.genSalt(saltRound)
+      .then( (salt) => {
+        bcrypt.hash(req.body.password, salt)
+          .then( (hash) => {
+            console.log(hash);
+            Users.create({
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              email: req.body.email,
+              password: hash,
+              user_role: req.body.user_role
+            }).then( () => {
+              console.log('Inserted new user');
+              res.end();
+            }).catch( (err) => {
+              console.log(err);
+            });
+          });
+      })
+      .catch( (err) => {
+        console.log(err);
+      });
+    res.redirect('/');
   });
+
+// router.route('/users')
+//   .post( (req, res) => {
+//     return Users.create({
+//       firstName: req.body.firstName,
+//       lastName: req.body.lastName,
+//       email: req.body.email,
+//       password: req.body.password,
+//       user_role: req.body.user_role
+//     })
+//     .then( (user) => {
+//       return res.json(user);
+//     });
+//   })
+//   .get( (req, res) => {
+//     Users.findAll()
+//     .then( (users) => {
+//       return res.json(users);
+//     });
+//   });
 
 router.route('/users/:id')
   .get( (req, res) => {
@@ -87,7 +91,9 @@ router.post('/login', (req, res) => {
     if (!user) {
       return res.json('Invalid Login');
     }
+    console.log('Login Successful');
     return res.json(user);
+
   });
 });
 
